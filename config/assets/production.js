7747feb62d6646af38c44db15e2c8c0d82308847
George Kachambwa <kachambwa.george@gmail.com>
2016-11-23 13:15:12 +0200

    Required Libs

--- config/assets/production.js
+++ config/assets/production.js
@@ -6,6 +6,10 @@ module.exports = {
       css: [
         'public/lib/bootstrap/dist/css/bootstrap.min.css',
         'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+        'public/lib/angular-xeditable/dist/css/xeditable.min.css',
+        'public/lib/hamburgers/dist/hamburgers.min.css',
+        'public/lib/textAngular/dist/textAngular.min.css',
+        'public/lib/font-awesome/css/font-awesome.min.css'
       ],
       js: [
         'public/lib/angular/angular.min.js',
@@ -17,7 +21,13 @@ module.exports = {
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
         'public/lib/angular-file-upload/angular-file-upload.min.js',
         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
-        'public/lib/bootstrap/dist/js/bootstrap.js'
+        'public/lib/angular-xeditable/dist/js/xeditable.min.js',
+        'public/lib/jquery/dist/jquery.min.js',
+        'public/lib/bootstrap/dist/js/bootstrap.js',
+        'public/lib/underscore/underscore-min.js',
+        'public/lib/textAngular/dist/textAngular-rangy.min.js',
+        'public/lib/textAngular/dist/textAngular-sanitize.min.js',
+        'public/lib/textAngular/dist/textAngular.min.js'
       ]
     },
     css: 'public/dist/application.min.css',
